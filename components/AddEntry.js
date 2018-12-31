de4eefada68f3bf303727d936f1adf7238c61d9d
Peter A. Higgins <pahiggins@cenabo.com>
2018-12-31 13:53:28 +0000

    Create TextButton component

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { View, TouchableOpacity, Text } from 'react-native';
 import { getMetricMetaInfo, timeToString } from '../utils/helpers';
+import { Ionicons } from '@expo/vector-icons';
 import EntrySlider from './EntrySlider';
 import EntryStepper from './EntryStepper';
 import DateHeader from './DateHeader';
+import TextButton from './TextButton';
 
 function SubmitBtn({ onPress }) {
   return (
@@ -73,8 +75,31 @@ export default class AddEntry extends Component {
     // Clear local notifications
   }
 
+  reset = () => {
+    const key = timeToString();
+
+    // Update Redux
+    // Navigate to home
+    // Update DB
+  }
+
   render() {
     const metaInfo = getMetricMetaInfo();
+
+    if (this.props.alreadyLogged) {
+      return (
+        <View>
+          <Ionicons
+            name='ios-happy'
+            size={100}
+          />
+          <Text>You already logged your information for today</Text>
+          <TextButton onPress={this.reset}>
+            Reset
+          </TextButton>
+        </View>
+      );
+    }
 
     return (
       <View>
