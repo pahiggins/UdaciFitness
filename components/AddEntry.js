63ac6ff2414240ba4bcfa9932c7d3daef34cbaa7
Peter A. Higgins <pahiggins@cenabo.com>
2019-01-03 12:17:28 +0000

    Setup redux

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { View, TouchableOpacity, Text } from 'react-native';
-import { getMetricMetaInfo, timeToString } from '../utils/helpers';
+import { connect } from 'react-redux';
+import { getMetricMetaInfo, timeToString, getDailyReminderValue } from '../utils/helpers';
 import { Ionicons } from '@expo/vector-icons';
 import EntrySlider from './EntrySlider';
 import EntryStepper from './EntryStepper';
 import DateHeader from './DateHeader';
 import TextButton from './TextButton';
 import { submitEntry, removeEntry } from '../utils/api';
+import { addEntry } from '../actions';
 
 function SubmitBtn({ onPress }) {
   return (
@@ -18,7 +20,7 @@ function SubmitBtn({ onPress }) {
   );
 }
 
-export default class AddEntry extends Component {
+class AddEntry extends Component {
   state = {
     run: 0,
     bike: 0,
@@ -63,7 +65,10 @@ export default class AddEntry extends Component {
     const key = timeToString();
     const entry = this.state;
 
-    // Update Redux
+    this.props.dispatch(addEntry({
+      [key]: entry
+    }));
+
     this.setState(() => ({
       run: 0,
       bike: 0,
@@ -81,7 +86,10 @@ export default class AddEntry extends Component {
   reset = () => {
     const key = timeToString();
 
-    // Update Redux
+    this.props.dispatch(addEntry({
+      [key]: getDailyReminderValue(),
+    }));
+
     // Navigate to home
     removeEntry(key);
   }
@@ -135,3 +143,13 @@ export default class AddEntry extends Component {
     );
   }
 }
+
+function mapStateToProps(state) {
+  const key = timeToString();
+
+  return {
+    alreadyLogged: state[key] && typeof state[key].today === 'undefined'
+  };
+}
+
+export default connect(mapStateToProps)(AddEntry);
--- utils/helpers.js
+++ utils/helpers.js
@@ -143,3 +143,9 @@ export function timeToString(time = Date.now()) {
   const todayUTC = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()))
   return todayUTC.toISOString().split('T')[0]
 }
+
+export function getDailyReminderValue() {
+  return {
+    today: '👋🏻 Do not forget to log your data today!',
+  };
+}
